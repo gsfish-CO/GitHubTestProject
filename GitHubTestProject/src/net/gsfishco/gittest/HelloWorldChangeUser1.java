a3a4ff4318d97168b04c5d88799c8229c74b4402
Gary Fish <nospam@gmail.com>
2018-11-05 19:21:20 -0700

    Check in initial hello world files.

---
+++ GitHubTestProject/src/net/gsfishco/gittest/HelloWorldChangeUser1.java
@@ -0,0 +1,11 @@
+package net.gsfishco.gittest;
+
+public class HelloWorldChangeUser1 {
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello and Good Morning World!");
+		}
+	}
+
+}
---
+++ GitHubTestProject/src/net/gsfishco/gittest/HelloWorldChangeUser2.java
@@ -0,0 +1,12 @@
+package net.gsfishco.gittest;
+
+public class HelloWorldChangeUser2 {
+	
+	public static void main(String[] args) {
+		String[] names = {"George", "Scott", "John"};
+
+		for (String name : names) {
+			System.out.println("Hello World from " + name + "!");
+		}
+	}
+}
